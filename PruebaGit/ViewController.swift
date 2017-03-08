21a7f5416b87fee4629521fb12fb7458260b249d
Kedfy José González Azuaje <kedfygo@iMac-de-Kedfy.local>
2017-03-08 09:39:05 -0600

    Initial Commit

---
+++ PruebaGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PruebaGit
+//
+//  Created by Kedfy José González Azuaje on 08/03/17.
+//  Copyright © 2017 Kedfy José González Azuaje. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
